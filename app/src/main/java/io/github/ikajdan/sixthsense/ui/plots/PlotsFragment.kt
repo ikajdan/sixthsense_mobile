02926161dd57cc407145e223f0a2b21b224ed44a
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2023-04-23 12:39:37 +0200

    Add AAInfographics library

--- app/src/main/java/io/github/ikajdan/sixthsense/ui/plots/PlotsFragment.kt
+++ app/src/main/java/io/github/ikajdan/sixthsense/ui/plots/PlotsFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartAnimationType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 import io.github.ikajdan.sixthsense.databinding.FragmentPlotsBinding
 
 class PlotsFragment : Fragment() {
@@ -22,16 +26,25 @@ class PlotsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val plotsViewModel =
-            ViewModelProvider(this).get(PlotsViewModel::class.java)
-
         _binding = FragmentPlotsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textPlots
-        plotsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val aaChartView = binding.aaChartView
+        val aaChartModel : AAChartModel = AAChartModel()
+            .chartType(AAChartType.Line)
+            .backgroundColor("#00000000")
+            .colorsTheme(arrayOf("#b41c56"))
+            .yAxisGridLineWidth(0)
+            .animationDuration(0)
+            .markerRadius(3)
+            .series(arrayOf(
+                AASeriesElement()
+                    .name("Tokyo")
+                    .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6))
+            )
+        )
+        aaChartView.aa_drawChartWithChartModel(aaChartModel)
+
         return root
     }
 
--- app/src/main/java/io/github/ikajdan/sixthsense/ui/settings/SettingsFragment.kt
+++ app/src/main/java/io/github/ikajdan/sixthsense/ui/settings/SettingsFragment.kt
@@ -22,16 +22,9 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSettings
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
