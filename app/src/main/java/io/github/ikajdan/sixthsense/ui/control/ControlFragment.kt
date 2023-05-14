11b3dff4ec4b0a1095153652a335574ff432bacb
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2023-05-14 12:21:18 +0200

    Add color picker to the control fragment

--- app/src/main/java/io/github/ikajdan/sixthsense/ui/control/ControlFragment.kt
+++ app/src/main/java/io/github/ikajdan/sixthsense/ui/control/ControlFragment.kt
@@ -1,30 +1,68 @@
 package io.github.ikajdan.sixthsense.ui.control
 
+import android.R
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.google.android.material.snackbar.Snackbar
+import com.madrapps.pikolo.ColorPicker
+import com.madrapps.pikolo.listeners.SimpleColorSelectionListener
 import io.github.ikajdan.sixthsense.databinding.FragmentControlBinding
 
+
 class ControlFragment : Fragment() {
-
     private var _binding: FragmentControlBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentControlBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.colorPicker.setColorSelectionListener(object : SimpleColorSelectionListener() {
+            override fun onColorSelected(color: Int) {
+                //binding.imageView.setColorFilter(color, PorterDuff.Mode.MULTIPLY)
+                binding.imageView.getBackground().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+                binding.ledColorInput.setText(String.format("%06X", 0xFFFFFF and color))
+            }
+        })
+
+        binding.ledPosXInput.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                binding.ledPosXInput.setText("")
+            }
+        }
+
+        binding.ledPosYInput.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                binding.ledPosYInput.setText("")
+            }
+        }
+
+        binding.requestButton.setOnClickListener {
+            requestRoutine()
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
     // Request routine
     private fun requestRoutine() {
-        val host = "http://" + "10.0.2.2"
-        val port = ":" + "8080"
+        val host = "http://" + "laptop.lan" + "/leds/set/"
+        val port = ":" + "8000"
         val reqX = "x=" + binding.ledPosXInput.text.toString()
         val reqY = "y=" + binding.ledPosYInput.text.toString()
         val hexColor =  binding.ledColorInput.text.toString()
@@ -35,8 +73,6 @@ class ControlFragment : Fragment() {
         var url = "$host$port/v1/set?$reqX&$reqY&$reqR&$reqG&$reqB".lowercase()
 
         if (url.isEmpty()) return
-
-        binding.textControl.text = ""
 
         // Prepend with the protocol, if necessary
         if (!url.startsWith("https://", true) && !url.startsWith("http://", true)) {
@@ -49,47 +85,17 @@ class ControlFragment : Fragment() {
         // Request a string response from the provided URL
         val stringRequest = StringRequest(
             Request.Method.GET, url,
-            { response ->
-                // Show the response
-                binding.textControl.text = response
+            {
             },
             {
                 // Display the error message
-                val msg = if (it.localizedMessage.isNullOrEmpty()) "Unknown Error" else it.localizedMessage
-                Snackbar.make(binding.root, msg, Snackbar.LENGTH_LONG)
-                    .setAnchorView(binding.requestButton).show()
+                // val msg = if (it.localizedMessage.isNullOrEmpty()) "Unknown Error" else it.localizedMessage
+                // Snackbar.make(binding.root, msg, Snackbar.LENGTH_LONG)
+                //     .setAnchorView(binding.requestButton).show()
             }
         )
 
         // Add the request to the RequestQueue
         queue.add(stringRequest)
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val controlViewModel =
-            ViewModelProvider(this)[ControlViewModel::class.java]
-
-        _binding = FragmentControlBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textControl
-        controlViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        binding.requestButton.setOnClickListener {
-            requestRoutine()
-        }
-
-        return root
-    }
-
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
 }
